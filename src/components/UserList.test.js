82888ca6a460a20da2fa341b6abeaec2d404dbff
u.plakhotnycha <u.plakhotnycha@plarium.com>
2024-04-19 15:23:42 +0200

    testing table contents

--- src/components/UserList.test.js
+++ src/components/UserList.test.js
@@ -13,14 +13,34 @@ test("render one row per user", () => {
       email: "sam@sam.com",
     },
   ];
-  const { container } = render(<UserList users={users} />);
+  render(<UserList users={users} />);
+  // screen.logTestingPlaygroundURL(); - tool, that helps to find rule for selector
 
   // Find all the rows in the table
-  // eslint-disable-next-line
-  const rows = container.querySelectorAll("tbody tr");
+  const rows = within(screen.getByTestId("users")).getAllByRole("row");
 
   // Assertion: correct number of rows in the table
   expect(rows).toHaveLength(2);
 });
 
-test("render the email and name of each user", () => {});
+test("render the email and name of each user", () => {
+  const users = [
+    {
+      name: "jane",
+      email: "jane@jane.com",
+    },
+    {
+      name: "sam",
+      email: "sam@sam.com",
+    },
+  ];
+  render(<UserList users={users} />);
+
+  for (let user of users) {
+    const name = screen.getByRole("cell", { name: user.name });
+    const email = screen.getByRole("cell", { name: user.email });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  }
+});
