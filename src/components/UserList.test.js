f2e676a54e75912d397e862073a4c128e2e7aed0
u.plakhotnycha <u.plakhotnycha@plarium.com>
2024-04-25 11:32:22 +0200

    DRY refactor

--- src/components/UserList.test.js
+++ src/components/UserList.test.js
@@ -1,8 +1,7 @@
 import { render, screen, within } from "@testing-library/react";
 import UserList from "./UserList";
 
-test("render one row per user", () => {
-  // Render the component
+function renderComponent() {
   const users = [
     {
       name: "jane",
@@ -14,6 +13,17 @@ test("render one row per user", () => {
     },
   ];
   render(<UserList users={users} />);
+
+  return {
+    users,
+  };
+}
+
+beforeEach()
+
+test("render one row per user", () => {
+  // Render the component
+  renderComponent();
   // screen.logTestingPlaygroundURL(); - tool, that helps to find rule for selector
 
   // Find all the rows in the table
@@ -24,17 +34,7 @@ test("render one row per user", () => {
 });
 
 test("render the email and name of each user", () => {
-  const users = [
-    {
-      name: "jane",
-      email: "jane@jane.com",
-    },
-    {
-      name: "sam",
-      email: "sam@sam.com",
-    },
-  ];
-  render(<UserList users={users} />);
+  const { users } = renderComponent();
 
   for (let user of users) {
     const name = screen.getByRole("cell", { name: user.name });
